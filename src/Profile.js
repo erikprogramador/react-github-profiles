491d8d14ba3420f10acc678eee1ee1819106981e
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-25 21:57:23 -0300

    Get the user information

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
+import axios from 'axios';
 
 class Profile extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      loading: true,
+      username: '',
+      profile: {},
+      repos: {}
+    }
+  }
+
+  componentWillMount () {
+    axios.get(`https://api.github.com/users/${this.props.match.params.username}`)
+      .then(response => {
+        // repos_url
+        this.setState({
+          loading: false,
+          username: this.props.match.params.username,
+          profile: response.data
+        });
+      })
+      .catch(console.error); // Redirect to welcome with error message
+  }
+
   render () {
+    if (this.state.loading) {
+      return (
+        <Loading />
+      );
+    }
+
     return (
       <div>
-        {this.props.match.params.username}
+        <img src={this.state.profile.avatar_url} alt={this.state.profile.name} title={this.state.profile.name} />
+        <h3>{this.state.profile.name}</h3>
+        <h5>{this.state.profile.login}</h5>
+        <p>{this.state.profile.bio}</p>
       </div>
     );
   }
